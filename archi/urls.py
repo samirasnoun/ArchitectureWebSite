dcb536863f509411250a431ba0128c27d7c9aedd
ASNOUN <samir.asnoun@gmail.com>
2016-07-26 17:04:35 +0200

    correction bug pg_cong et sitemap dans urls

--- archi/urls.py
+++ archi/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from website.views import *
-from website.sitemap import *
+#from website.sitemap import *
 from django.contrib.sitemaps.views import sitemap
 from website.sitemaps import ChantierSitemap, MediaSitemap,  StaticViewSitemap
 
